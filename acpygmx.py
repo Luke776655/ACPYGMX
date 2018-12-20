e993327e3e2b12436f06b6b9bbc5d2a7af7f9ac6
Luke001 <lukasz.radzinski@gmail.com>
2018-12-20 19:17:25 +0100

    komentarze i strukturyzacja

--- acpygmx.py
+++ acpygmx.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3.5
 
+#importowanie modułów
 import sys
 import subprocess
-print(sys.version)
 
-def helpprint():
+#funkcja wyświetlająca pomoc
+def print_help():
 	print("ACPYGMX v. 0.0\n")
 	print("Program usage:\n")
 	print("acpygmx.py -f _inputname.pdb_ [-o _topologyname.top_]")
@@ -13,112 +14,149 @@ def helpprint():
 	print("Options:\n\t-f _inputname.pdb_\tname of input PDB file\n\t-o _topologyname.top_\tname of output topology file\n\t-h\t\t\thelp")
 	sys.exit()
 
-def createGMXdatabase(path):
-	residatabase = []
+#funkcja pobierająca bazę danych reszt chemicznych z GROMACS
+def get_gmx_resi_database(path_to_ff):
+	resi_database = []
 	try:
-		aa_resi = open(path + "//aminoacids.hdb", "r").read().split("\n")
+		aa_resi = open(path_to_ff + "//aminoacids.hdb", "r").read().split("\n")
 	except:
 		print("\nERROR: No such file in directory\n")
-		helpprint()
+		print_help()
 	for line in aa_resi:
 		line = line.split("\t")
 		if(len(line[0])>2 and len(line[0])<6 and line[0][0].isupper() == True):
-			residatabase.append(line[0])
-	residatabase.append('HIS')
+			resi_database.append(line[0])
+	resi_database.append('HIS')
+
 	try:
-		dna_resi = open(path + "//dna.hdb", "r").read().split("\n")
+		dna_resi = open(path_to_ff + "//dna.hdb", "r").read().split("\n")
 	except:
 		print("\nERROR: No such file in directory\n")
-		helpprint()
+		print_help()
 	for line in dna_resi:
 		line = line.split("\t")
 		if(len(line[0])>2 and len(line[0])<6 and line[0][0].isupper() == True):
-			residatabase.append(line[0])
+			resi_database.append(line[0])
+
 	try:
-		rna_resi = open(path + "//rna.hdb", "r").read().split("\n")
+		rna_resi = open(path_to_ff + "//rna.hdb", "r").read().split("\n")
 	except:
 		print("\nERROR: No such file in directory\n")
-		helpprint()
+		print_help()
 	for line in rna_resi:
 		line = line.split("\t")
 		if(len(line[0])>2 and len(line[0])<6 and line[0][0].isupper() == True):
-			residatabase.append(line[0])
-	return residatabase
+			resi_database.append(line[0])
+	return resi_database
 
-def writenonstdpdb(resi, pdb_org):
-	for i in resi:
+#funkcja zapisująca wybrane reszty chemiczne do plików PDB
+def write_resi_to_pdb(resis, source_pdb):
+	for i in resis:
 		subprocess.run(["mkdir", i])
 		try:
 			resi_pdb = open('./'+i+'/'+i+'.pdb', "w+")
 		except:
 			print("\nERROR: Cannot create a file\n")
-			helpprint()
+			print_help()
 		resi_pdb.write("HEADER " + i + '\n')
-		for j in range(2, len(resi[i]), 1):
-			resi_pdb.write(pdb_org[resi[i][j]] + '\n')
+		for j in range(len(resis[i].record_line)):
+			resi_pdb.write(source_pdb[resis[i].record_line[j]] + '\n')
 		resi_pdb.write("END")
 		resi_pdb.close()
 
-def createnonstdpdbtopology(resi):
-	for i in resi:
+#funkcja tworząca topologię dla wybranych reszt chemicznych
+def make_resi_topology(resis):
+	for i in resis:
+		#otwieranie skryptu dodającego wodory
 		subprocess.run(["babel", "-ipdb", i+".pdb", "-opdb", i+"_h.pdb", "-h"], cwd='./'+i)
+		#otwieranie skryptu generującego topologię
 		subprocess.run(["acpype", "-i", i+"_h.pdb"], cwd='./'+i)
 
+#klasa ze strukturą wiersza PDB
+class PdbLine:
+	def __init__(self):
+		self.PdbLine = []
+	is_record_atomic = False
+	record_name = ''
+	atom_id = 0
+	atom_name = ''
+	resi_name = ''
+	chain = ''
+	resi_id = 0
+	#funkcja przypisująca wartości zmiennym
+	def get_it(self, line):
+		if(len(line)>=50):
+			self.record_name = line[0:6].strip()
+		if(self.record_name == 'ATOM' or self.record_name == 'HETATM'):
+			self.is_record_atomic = True
+			self.atom_id = int(line[6:11])
+			self.atom_name = line[12:16].strip()
+			self.resi_name = line[17:20].strip()
+			self.chain = line[21]
+			self.resi_id = int(line[22:26])
+
+#klasa ze strukturą lokalizacji wybranej reszty chemicznej
+class ResiLocation:
+	def __init__(self):
+		self.ResiLocation = []
+	chain = ''
+	resi_id = 0
+	record_line = []
+
+
+#START PROGRAMU
+
+#Sprawdzanie poprawności argumentów wejściowych programu
 if(len(sys.argv)>5):
 	print("\nERROR: To much input values\n")
-	helpprint()
+	print_help()
 
-pdb_name = ''
-topol_name = 'topol.top'
-nonstdresi = {}
+source_pdb_name = ''
+topology_name = 'topol.top'
+nonstandard_resi = {}
 
-
+#obsługa opcji wejściowych
 for i in range(len(sys.argv)):
 	if(sys.argv[i] == '-h'):
-		helpprint()
+		print_help()
 	elif(sys.argv[i] == '-f' and sys.argv[i+1][0] != '-'):
-		pdb_name = sys.argv[i+1]
+		source_pdb_name = sys.argv[i+1]
 	elif(sys.argv[i] == '-o' and sys.argv[i+1][0] != '-'):
-		topol_name = sys.argv[i+1]
+		topology_name = sys.argv[i+1]
 	elif(sys.argv[i][0] == '-'):
 		print("\nERROR:Unknown option: " + sys.argv[i] + "\n")
-		helpprint()
+		print_help()
 
-print("PDB file name: " + pdb_name)
-print("Topology file name: " + topol_name)
+print("PDB file name: " + source_pdb_name)
+print("Topology file name: " + topology_name)
 
+#otwieranie źródłowego pliku PDB
 try:
-	pdb_file = open(pdb_name, "r").read().split("\n")
+	source_pdb = open(source_pdb_name, "r").read().split("\n")
 except:
 	print("\nERROR: No such file in directory\n")
-	helpprint()
+	print_help()
 
-GMXPATH = "./amber99sb-ildn.ff"
-GMXdatabase = createGMXdatabase(GMXPATH)
+ff_path = "./amber99sb-ildn.ff"
+GMXdatabase = get_gmx_resi_database(ff_path)
 
-i = 0
-for line in pdb_file:
-	line = line.split()
-	if(len(line)>=8  and (line[0] == 'ATOM' or line[0] == 'HETATM')):
-		if(((line[3] in GMXdatabase) == False) and ((line[3] in nonstdresi) == False)):
-			if(line[4].isupper() == True):
-				nonstdresi[line[3]] = [(line[4])]
-				nonstdresi[line[3]].append(int(line[5]))
-			else:
-				nonstdresi[line[3]] = [0]
-				nonstdresi[line[3]].append(int(line[4]))
-			nonstdresi[line[3]].append(i)
-		elif((line[3] in nonstdresi) == True) :
-				if(line[4].isupper() == True):
-					if(nonstdresi[line[3]][0] == line[4] and nonstdresi[line[3]][1] == int(line[5])):
-						nonstdresi[line[3]].append(i)
-				else:
-					if(nonstdresi[line[3]][0] == 0 and nonstdresi[line[3]][1] == int(line[4])):
-						nonstdresi[line[3]].append(i)
-	i+=1
+#wyszukiwanie niestandardowych reszt chemicznych
+for i in range(len(source_pdb)):
+	pdb_line = PdbLine()
+	pdb_line.get_it(source_pdb[i])
+	if(pdb_line.is_record_atomic == True):
+		if((pdb_line.resi_name in GMXdatabase) == False and (pdb_line.resi_name in nonstandard_resi) == False):
+			nonstandard_resi[pdb_line.resi_name] = ResiLocation()
+			nonstandard_resi[pdb_line.resi_name].chain = pdb_line.chain
+			nonstandard_resi[pdb_line.resi_name].resi_id = pdb_line.resi_id
+			nonstandard_resi[pdb_line.resi_name].record_line.append(i)
+		elif((pdb_line.resi_name in nonstandard_resi) == True):
+			if(nonstandard_resi[pdb_line.resi_name].chain == pdb_line.chain and nonstandard_resi[pdb_line.resi_name].resi_id == pdb_line.resi_id):
+				nonstandard_resi[pdb_line.resi_name].record_line.append(i)
 
-for k in nonstdresi:
-	print(k, nonstdresi[k], '\n')
 
-writenonstdpdb(nonstdresi, pdb_file)
-createnonstdpdbtopology(nonstdresi)
+for k in nonstandard_resi:
+	print(k, nonstandard_resi[k], '\n')
+
+write_resi_to_pdb(nonstandard_resi, source_pdb)
+make_resi_topology(nonstandard_resi)
