88d0acf7ecf92b7c1d42366ed1092db350f9ac13
Luke001 <lukasz.radzinski@gmail.com>
2018-12-21 16:11:19 +0100

    Tworzenie topoogii cząsteczek z ładunkiem

--- acpygmx.py
+++ acpygmx.py
@@ -18,59 +18,39 @@ def print_help():
 def get_gmx_resi_database(path_to_ff):
 	resi_database = []
 	try:
-		aa_resi = open(path_to_ff + "//aminoacids.hdb", "r").read().split("\n")
+		aa_resi = open(path_to_ff + "//aminoacids.rtp", "r").read().split("\n")
 	except:
 		print("\nERROR: No such file in directory\n")
 		print_help()
 	for line in aa_resi:
-		line = line.split("\t")
-		if(len(line[0])>2 and len(line[0])<6 and line[0][0].isupper() == True):
-			resi_database.append(line[0])
+		if(len(line)>4 and line[0] == '['):
+			line = line.split()
+			if(len(line[1])>=1 and len(line[1])<=3):
+				resi_database.append(line[1])
 	resi_database.append('HIS')
 
 	try:
-		dna_resi = open(path_to_ff + "//dna.hdb", "r").read().split("\n")
+		dna_resi = open(path_to_ff + "//dna.rtp", "r").read().split("\n")
 	except:
 		print("\nERROR: No such file in directory\n")
 		print_help()
 	for line in dna_resi:
-		line = line.split("\t")
-		if(len(line[0])>2 and len(line[0])<6 and line[0][0].isupper() == True):
-			resi_database.append(line[0])
+		if(len(line)>4 and line[0] == '['):
+			line = line.split()
+			if(len(line[1])>=1 and len(line[1])<=3):
+				resi_database.append(line[1])
 
 	try:
-		rna_resi = open(path_to_ff + "//rna.hdb", "r").read().split("\n")
+		rna_resi = open(path_to_ff + "//rna.rtp", "r").read().split("\n")
 	except:
 		print("\nERROR: No such file in directory\n")
 		print_help()
-	for line in rna_resi:
-		line = line.split("\t")
-		if(len(line[0])>2 and len(line[0])<6 and line[0][0].isupper() == True):
-			resi_database.append(line[0])
+	for line in dna_resi:
+		if(len(line)>4 and line[0] == '['):
+			line = line.split()
+			if(len(line[1])>=1 and len(line[1])<=3):
+				resi_database.append(line[1])
 	return resi_database
-
-#funkcja zapisująca wybrane reszty chemiczne do plików PDB
-def write_resi_to_pdb(resis, source_pdb):
-	for i in resis:
-		subprocess.run(["mkdir", i])
-		try:
-			resi_pdb = open('./'+i+'/'+i+'.pdb', "w+")
-		except:
-			print("\nERROR: Cannot create a file\n")
-			print_help()
-		resi_pdb.write("HEADER " + i + '\n')
-		for j in range(len(resis[i].record_line)):
-			resi_pdb.write(source_pdb[resis[i].record_line[j]] + '\n')
-		resi_pdb.write("END")
-		resi_pdb.close()
-
-#funkcja tworząca topologię dla wybranych reszt chemicznych
-def make_resi_topology(resis):
-	for i in resis:
-		#otwieranie skryptu dodającego wodory
-		subprocess.run(["babel", "-ipdb", i+".pdb", "-opdb", i+"_h.pdb", "-h"], cwd='./'+i)
-		#otwieranie skryptu generującego topologię
-		subprocess.run(["acpype", "-i", i+"_h.pdb"], cwd='./'+i)
 
 #klasa ze strukturą wiersza PDB
 class PdbLine:
@@ -83,9 +63,10 @@ class PdbLine:
 	resi_name = ''
 	chain = ''
 	resi_id = 0
+	charge = 0
 	#funkcja przypisująca wartości zmiennym
 	def get_it(self, line):
-		if(len(line)>=50):
+		if(len(line)>=70):
 			self.record_name = line[0:6].strip()
 		if(self.record_name == 'ATOM' or self.record_name == 'HETATM'):
 			self.is_record_atomic = True
@@ -94,6 +75,12 @@ class PdbLine:
 			self.resi_name = line[17:20].strip()
 			self.chain = line[21]
 			self.resi_id = int(line[22:26])
+			if(line[78].isdigit() == True):
+				if(line[79] == '+'):
+					self.charge = int(line[78])
+				elif(line[79] == '-'):
+					self.charge = -1*int(line[78])
+			
 
 #klasa ze strukturą lokalizacji wybranej reszty chemicznej
 class ResiLocation:
@@ -101,7 +88,65 @@ class ResiLocation:
 		self.ResiLocation = []
 	chain = ''
 	resi_id = 0
-	record_line = []
+	record_lines = []
+
+#funkcja wyszukująca niestandardowe reszty chemiczne w pliku PDB, zwraca je i ich pozycje jako słownik
+def get_nonstandard_resis(pdb, database):
+	resis = {}
+	for i in range(len(pdb)):
+		line = PdbLine()
+		line.get_it(pdb[i])
+		if(line.is_record_atomic == True):
+			if((line.resi_name in database) == False and (line.resi_name in resis) == False):
+				resis[line.resi_name] = ResiLocation()
+				resis[line.resi_name].chain = line.chain
+				resis[line.resi_name].resi_id = line.resi_id
+				resis[line.resi_name].record_lines = []
+				resis[line.resi_name].record_lines.append(i)
+			elif((line.resi_name in resis) == True):
+				if(resis[line.resi_name].chain == line.chain and resis[line.resi_name].resi_id == line.resi_id):
+					resis[line.resi_name].record_lines.append(i)
+	return resis
+
+#funkcja wczytująca wybrane reszty chemiczne z pliku PDB i zapisująca je do osobnych plików PDB
+def split_pdb_by_resi(pdb, resis):
+	for i in resis:
+		subprocess.run(["mkdir", i])
+		try:
+			resi_pdb = open('./'+i+'/'+i+'_no_H.pdb', "w+")
+		except:
+			print("\nERROR: Cannot create a file\n")
+			print_help()
+		resi_pdb.write("HEADER " + i + '\n')
+		for j in range(len(resis[i].record_lines)):
+			resi_pdb.write(source_pdb[resis[i].record_lines[j]] + '\n')
+		resi_pdb.write("END")
+		resi_pdb.close()
+
+#funkcja odczytująca ładunek związku z pliku PDB
+def get_charge_from_pdb(pdb_path):
+	charge = 0
+	try:
+		molecule = open(pdb_path, "r").read().split("\n")
+	except:
+		print("\nERROR: No such file in directory\n")
+		print_help()
+	for i in molecule:
+		line = PdbLine()
+		line.charge = 0
+		line.get_it(i)
+		charge += line.charge
+	return charge
+
+#funkcja tworząca topologię dla wybranych reszt chemicznych
+def make_resi_topology(resis):
+	for i in resis:
+		#otwieranie skryptu dodającego wodory
+		subprocess.run(["babel", "-ipdb", i+"_no_H.pdb", "-opdb", i+".pdb", "-p"], cwd='./'+i)
+		#odczytanie ładunku uwodornionej reszty chemicznej
+		charge = get_charge_from_pdb('./'+i+'/'+i+'.pdb')
+		#otwieranie skryptu generującego topologię
+		subprocess.run(["acpype", "-i", i+".pdb", "-n", str(charge)], cwd='./'+i)
 
 
 #START PROGRAMU
@@ -113,7 +158,7 @@ if(len(sys.argv)>5):
 
 source_pdb_name = ''
 topology_name = 'topol.top'
-nonstandard_resi = {}
+nonstandard_resis = {}
 
 #obsługa opcji wejściowych
 for i in range(len(sys.argv)):
@@ -137,26 +182,19 @@ except:
 	print("\nERROR: No such file in directory\n")
 	print_help()
 
-ff_path = "./amber99sb-ildn.ff"
-GMXdatabase = get_gmx_resi_database(ff_path)
+#pobieranie bazy danych reszt chemicznych z GROMACS
+amberff_path = "./amber99sb-ildn.ff"
+gmx_database = get_gmx_resi_database(amberff_path)
 
 #wyszukiwanie niestandardowych reszt chemicznych
-for i in range(len(source_pdb)):
-	pdb_line = PdbLine()
-	pdb_line.get_it(source_pdb[i])
-	if(pdb_line.is_record_atomic == True):
-		if((pdb_line.resi_name in GMXdatabase) == False and (pdb_line.resi_name in nonstandard_resi) == False):
-			nonstandard_resi[pdb_line.resi_name] = ResiLocation()
-			nonstandard_resi[pdb_line.resi_name].chain = pdb_line.chain
-			nonstandard_resi[pdb_line.resi_name].resi_id = pdb_line.resi_id
-			nonstandard_resi[pdb_line.resi_name].record_line.append(i)
-		elif((pdb_line.resi_name in nonstandard_resi) == True):
-			if(nonstandard_resi[pdb_line.resi_name].chain == pdb_line.chain and nonstandard_resi[pdb_line.resi_name].resi_id == pdb_line.resi_id):
-				nonstandard_resi[pdb_line.resi_name].record_line.append(i)
+nonstandard_resis = get_nonstandard_resis(source_pdb, gmx_database)
 
+#wyświetlanie niestandardowych reszt cheemicznych
+for i in nonstandard_resis:
+	print(i, nonstandard_resis[i].chain, nonstandard_resis[i].resi_id, nonstandard_resis[i].record_lines, '\n')
 
-for k in nonstandard_resi:
-	print(k, nonstandard_resi[k], '\n')
+#wczytywanie niestandardowych reszt chemicznych z pliku PDB i zapisywanie ich do osobnych plików PDB
+split_pdb_by_resi(source_pdb, nonstandard_resis)
 
-write_resi_to_pdb(nonstandard_resi, source_pdb)
-make_resi_topology(nonstandard_resi)
+#tworzenie topologii dla niestandardowych reszt chemicznych
+make_resi_topology(nonstandard_resis)
